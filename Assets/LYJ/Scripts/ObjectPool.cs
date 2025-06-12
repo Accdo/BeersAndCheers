efc6bde4fe360c0eaeaf4bb86b4daa9082e894bd
LEEYONGJAE113 <ionsi1103@gmail.com>
2025-06-12 15:29:12 +0900

    Create EventManager, ObjectPool and PoolManager Script

---
+++ Assets/LYJ/Scripts/ObjectPool.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    private GameObject prefab;
+    private Queue<GameObject> pool;
+
+    private Transform parent;
+
+    public ObjectPool(GameObject prefab, int size, Transform parent = null)
+    {
+        this.prefab = prefab;
+        this.parent = parent;
+        pool = new Queue<GameObject>();
+
+        for (int i = 0; i < size; i++)
+        {
+            CreateNewObject();
+        }
+    }
+
+    private void CreateNewObject()
+    {
+        GameObject obj = GameObject.Instantiate(prefab, parent);
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+
+    public GameObject GetObject()
+    {
+        if (pool.Count == 0)
+        {
+            CreateNewObject();
+        }
+
+        GameObject obj = pool.Dequeue();
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
