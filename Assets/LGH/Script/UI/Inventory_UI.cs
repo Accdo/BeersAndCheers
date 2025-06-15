a72490b9f001e4a3c54be5abca416a02346221c7
Accdo <khyeong1002@gmail.com>
2025-06-15 09:12:17 +0900

    인벤토리 드래그앤드롭 구현
    인벤토리 내에서 아이템 이동 구현
    인벤토리와 상시 인벤토리 간에 아이템 이동 구현
    임시 쓰레기통 구현

--- Assets/LGH/Script/UI/Inventory_UI.cs
+++ Assets/LGH/Script/UI/Inventory_UI.cs
@@ -1,47 +1,39 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Inventory_UI : MonoBehaviour
 {
-    public GameObject inventoryPanel;
-
-    public GH_Player player;
-
+    public string inventoryName;
     public List<Slot_UI> slots = new List<Slot_UI>();
+    [SerializeField] private Canvas canvas;
+    private Inventory inventory;
 
-    void Update()
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            ToggleInventory();
-        }
+        canvas = FindAnyObjectByType<Canvas>();
     }
 
-    public void ToggleInventory()
+    private void Start()
     {
-        if (!inventoryPanel.activeSelf)
-        {
-            inventoryPanel.SetActive(true);
-            Refresh();
-        }
-        else
-        {
-            inventoryPanel.SetActive(false);
-        }
+        inventory = GH_GameManager.instance.player.inventory.GetInventoryByName(inventoryName);
+
+        SetupSlots();
+        Refresh();
     }
 
     // 새로고침
-    private void Refresh()
+    public void Refresh()
     {
         // 슬롯 갯수가 같다면
-        if (slots.Count == player.inventory.slots.Count)
+        if (slots.Count == inventory.slots.Count)
         {
             for (int i = 0; i < slots.Count; i++)
             {
                 // 플레이어가 가진 인벤토리 슬롯에 아이템이 존재 하는 경우
-                if (player.inventory.slots[i].itemName != "")
+                if (inventory.slots[i].itemName != "")
                 {
-                    slots[i].SetItem(player.inventory.slots[i]);
+                    slots[i].SetItem(inventory.slots[i]);
                 }
                 else // 빈 슬롯
                 {
@@ -51,15 +43,89 @@ public class Inventory_UI : MonoBehaviour
         }
     }
 
-    public void Remove(int slotID)
+    public void Remove()
     {
-        Item itemToDrop = GH_GameManager.instance.itemManager.GetItemByType(
-            player.inventory.slots[slotID].itemName);
+        Item itemToDrop = GH_GameManager.instance.itemManager.GetItemByName(
+            inventory.slots[UI_Manager.draggedSlot.slotID].itemName);
 
         if (itemToDrop != null)
         {
-            player.inventory.Remove(slotID);
+            if (UI_Manager.dragSingle)
+            {
+                inventory.Remove(UI_Manager.draggedSlot.slotID);
+            }
+            else
+            {
+                inventory.Remove(UI_Manager.draggedSlot.slotID, inventory.slots[UI_Manager.draggedSlot.slotID].count);
+            }
             Refresh();
+        }
+
+        UI_Manager.draggedSlot = null;
+    }
+
+    public void SlotBeginDrag(Slot_UI slot)
+    {
+        UI_Manager.draggedSlot = slot;
+
+        UI_Manager.draggedIcon = Instantiate(slot.itemIcon);
+        UI_Manager.draggedIcon.transform.SetParent(canvas.transform);
+        UI_Manager.draggedIcon.raycastTarget = false;
+        UI_Manager.draggedIcon.rectTransform.sizeDelta = new Vector2(80, 80);
+
+        MoveToMousePosition(UI_Manager.draggedIcon.gameObject);
+    }
+
+    public void SlotDrag()
+    {
+        MoveToMousePosition(UI_Manager.draggedIcon.gameObject);
+    }
+
+    public void SlotEndDrag()
+    {
+        Destroy(UI_Manager.draggedIcon.gameObject);
+        UI_Manager.draggedIcon = null;
+    }
+
+    public void SlotDrop(Slot_UI slot)
+    {
+        if (UI_Manager.dragSingle)
+        {
+            // 드래그 선택한 슬롯에서 드롭 시킨 슬롯으로 이동
+            UI_Manager.draggedSlot.inventory.MoveSlot(UI_Manager.draggedSlot.slotID, slot.slotID, slot.inventory);
+        }
+        else
+        {
+            UI_Manager.draggedSlot.inventory.MoveSlot(UI_Manager.draggedSlot.slotID, slot.slotID, slot.inventory,
+                UI_Manager.draggedSlot.inventory.slots[UI_Manager.draggedSlot.slotID].count);
+        }
+
+        GH_GameManager.instance.uiManager.RefreshAll();
+    }
+
+    private void MoveToMousePosition(GameObject toMove)
+    {
+        if (canvas != null)
+        {
+            Vector2 position;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform,
+                Input.mousePosition, null, out position);
+
+            toMove.transform.position = canvas.transform.TransformPoint(position);
+        }
+    }
+
+    // 슬롯 아이디 초기화 
+    private void SetupSlots()
+    {
+        int counter = 0;
+
+        foreach (Slot_UI slot in slots)
+        {
+            slot.slotID = counter;
+            counter++;
+            slot.inventory = inventory;
         }
     }
 }
