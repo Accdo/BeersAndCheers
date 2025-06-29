b68848f4136d6768577b9241680b2ff8ac8f730b
Accdo <khyeong1002@gmail.com>
2025-06-29 16:50:17 +0900

    기타 예외처리 및 되팔기 구현
    인벤토리 꽉참 예외처리,
    돈 부족 예외처리 내부 구현 완성
    쓰레기통 되팔기으로 수정

--- Assets/MHU/Scripts/Interactive/Washer.cs
+++ Assets/MHU/Scripts/Interactive/Washer.cs
@@ -3,14 +3,14 @@ using static Inventory;
 
 public class Washer : MonoBehaviour,IInteractable
 {
-    #region »óÈ£ÀÛ¿ë
+    #region ï¿½ï¿½È£ï¿½Û¿ï¿½
     public string GetCursorType() => "Washer";
     public string GetInteractionID() => "Washer";
     public InteractionType GetInteractionType() => InteractionType.MiniGame;
     #endregion
 
-    [SerializeField] private int useCount = 5; // »ç¿ë È½¼ö
-    // ¹° ÇÁ¸®ÆÕ
+    [SerializeField] private int useCount = 5; // ï¿½ï¿½ï¿½ È½ï¿½ï¿½
+    // ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
     [SerializeField] private GameObject washerWater;
 
     public WashingMinigame washingMinigame;
@@ -31,11 +31,12 @@ public class Washer : MonoBehaviour,IInteractable
         {
             if (slot.itemName == "WaterPail")
             {
-                // ¹° »ý¼º
+                // ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
                 SoundManager.Instance.Play("WaterSFX");
                 washerWater.SetActive(true);
                 useCount += 5;
-                GH_GameManager.instance.player.inventory.hotbar.CanRemove("WaterPail"); 
+                //GH_GameManager.instance.player.inventory.hotbar.CanRemove("WaterPail"); 
+                GH_GameManager.instance.player.inventory.RemoveItem("WaterPail");
             }
         }
         else
@@ -44,7 +45,7 @@ public class Washer : MonoBehaviour,IInteractable
             washingMinigame.WashingMiniGameStart();
             if(useCount == 0)
             {
-                // ¹° »èÁ¦
+                // ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
                 washerWater.SetActive(false);
             }
         }
